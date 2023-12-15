82ed05aa4f6c6e8ded9da528dec83dd427b99ca4
Augusto <augustocatterino@gmail.com>
2023-12-15 17:05:17 -0300

    posts, home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import {useDispatch} from "react-redux"
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
 
+
+
 import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
+import { fetchPosts } from "../redux/slices/post";
 
 // Home component
 export const Home = () => {
+  const dispatch = useDispatch();
+
+React.useEffect(() => {
+  dispatch(fetchPosts());
+}, [dispatch]);
   return (
     <>
       <Tabs
